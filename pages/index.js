b1030a79bbf04977061b51e38e4aa127dd09381f
Fortune <fortunemuhumuza@gmail.com>
2021-09-21 00:28:33 -0700

    initial commit

--- pages/index.js
+++ pages/index.js
@@ -1,209 +1,190 @@
-import Head from 'next/head'
+import { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css'
 
-export default function Home() {
+
+export const getStaticProps = async () => {
+  const res = await fetch('https://jsonplaceholder.typicode.com/users');
+  const data = await res.json()
+
+  console.log(data)
+
+  return {
+    props: {
+      users: data
+    }
+  }
+}
+
+
+
+
+
+
+
+
+export default function Home({users}) {
+
+  // const [users, setUsers] = useState([])
+  const [phoneNumber, setPhoneNumber] = useState('')
+
+  const apiKey = 'ac4c9b0e-ecbc-43c3-a785-a6480df2092f';
+
+  const getData = async () => {
+    const res = await fetch('https://jsonplaceholder.typicode.com/users');
+    const data = await res.json()
+  
+    console.log(data)
+    // let phoneNo = phoneNumber
+    //   let response = await axios.get(`https://api.kelvindata.com/rest/v1/search-v2?phone=${phoneNo}&apiKey=${apiKey}`, {
+
+    //   })
+
+    //   if(response){
+    //     setDetails(response.data)
+    //   }
+    //   else{
+        
+    //   }
+
+    //   console.log(response.data)
+
+  }
+
   return (
-    <div className="container">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main>
-        <h1 className="title">
-          Learn <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-
-        <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+    <div className="app">
+    <div className="container m-5 p-2 rounded mx-auto bg-light shadow">
+      {/* App title section */}
+      <div className="row m-1 p-4">
+        <div className="col">
+          <div className="p-1 h1 text-primary text-center mx-auto display-inline-block">
+            <i className="fa fa-check bg-primary text-white rounded p-2" />
+            <u>Online directory</u>
+          </div>
         </div>
-      </main>
+      </div>
 
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
-        </a>
-      </footer>
+      <div className="row m-1 p-3">
+        <div className="col col-11 mx-auto">
+          <div className="row bg-white rounded shadow-sm p-2 add-todo-wrapper align-items-center justify-content-center">
+            <div className="col">
+              <input className="form-control form-control-lg border-0  bg-transparent rounded" type="text"  placeholder="Enter phone number .."  />
+            </div>
 
-      <style jsx>{`
-        .container {
-          min-height: 100vh;
-          padding: 0 0.5rem;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
+            <div className="col-auto px-0 mx-0 mr-2">
+              <button type="button" className="btn btn-primary" onClick={getData}>Search</button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="p-2 mx-4 border-black-25 border-bottom" />
+      {/* View options section */}
+      <div className="row m-1 p-3 px-5 justify-content-end">
+        <div className="col-auto d-flex align-items-center">
+          <label className="text-secondary my-2 pr-2 view-opt-label">Filter</label>
+          <select className="custom-select custom-select-sm btn my-2">
+            <option value="all" selected>All</option>
+            <option value="completed">Male</option>
+            <option value="active">Female</option>
+          </select>
+        </div>
+        <div className="col-auto d-flex align-items-center px-1 pr-3">
+          <label className="text-secondary my-2 pr-2 view-opt-label">Sort</label>
+          <select className="custom-select custom-select-sm btn my-2">
+            <option value="added-date-asc" selected>Added date</option>
+            <option value="due-date-desc">Due date</option>
+          </select>
+          <i className="fa fa fa-sort-amount-asc text-info btn mx-0 px-0 pl-1" data-toggle="tooltip" data-placement="bottom" title="Ascending" />
+          <i className="fa fa fa-sort-amount-desc text-info btn mx-0 px-0 pl-1 d-none" data-toggle="tooltip" data-placement="bottom" title="Descending" />
+        </div>
+      </div>
 
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
+      <div className="row mx-1 px-5 pb-3 w-80">
+       
 
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
+        <table id="customers">
+<tr>
+  <th>First Name</th>
+  <th>Last Name</th>
+  <th>Gender</th>
+  <th style={{width: '200px'}}>Email(s)</th>
+  <th>Phone No.</th>
+</tr>
+{users.length > 0? users.map(detail => (
+              
+              <tbody>
+               
+                <tr>
+                  <td>{detail.name.first}</td>
+                  <td>{detail.name.last}</td>
+                  <td>{detail.gender}</td>
+                  <td>{detail.email}</td>
+                  <td>{detail.phone}</td>
+                </tr>
 
-        footer img {
-          margin-left: 0.5rem;
-        }
+              </tbody>
+              )): <h1>Sorry, no records were found for that number</h1>}
 
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
+</table>
 
-        a {
-          color: inherit;
-          text-decoration: none;
-        }
+{/* 
+          <table className="table w-90">
+            <thead className="thead-dark">
+              <tr>
+                <th scope="col">No.</th>
+                <th scope="col">First Name</th>
+                <th scope="col">Last Name</th>
+                <th scope="col">Gender</th>
+                <th scope="col" style={{width: '150px'}}>Email</th>
+                <th scope="col">Phone No.</th>
+              </tr>
+            </thead>
+            {details.map(detail => (
+              
+            <tbody>
+             
+              <tr>
+                <th scope="row">1</th>
+                <td>{detail.name.first}</td>
+                <td>{detail.name.last}</td>
+                <td>{detail.gender}</td>
+                <td>{detail.emailAddresses}</td>
+                <td>{detail.phoneAccounts.uri}</td>
+              </tr>
 
-        .title a {
-          color: #0070f3;
-          text-decoration: none;
-        }
+            </tbody>
+            ))}
+          </table> */}
+       
 
-        .title a:hover,
-        .title a:focus,
-        .title a:active {
-          text-decoration: underline;
-        }
 
-        .title {
-          margin: 0;
-          line-height: 1.15;
-          font-size: 4rem;
-        }
+       
 
-        .title,
-        .description {
-          text-align: center;
-        }
-
-        .description {
-          line-height: 1.5;
-          font-size: 1.5rem;
-        }
-
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-        }
-
-        .grid {
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          flex-wrap: wrap;
-
-          max-width: 800px;
-          margin-top: 3rem;
-        }
-
-        .card {
-          margin: 1rem;
-          flex-basis: 45%;
-          padding: 1.5rem;
-          text-align: left;
-          color: inherit;
-          text-decoration: none;
-          border: 1px solid #eaeaea;
-          border-radius: 10px;
-          transition: color 0.15s ease, border-color 0.15s ease;
-        }
-
-        .card:hover,
-        .card:focus,
-        .card:active {
-          color: #0070f3;
-          border-color: #0070f3;
-        }
-
-        .card h3 {
-          margin: 0 0 1rem 0;
-          font-size: 1.5rem;
-        }
-
-        .card p {
-          margin: 0;
-          font-size: 1.25rem;
-          line-height: 1.5;
-        }
-
-        .logo {
-          height: 1em;
-        }
-
-        @media (max-width: 600px) {
-          .grid {
-            width: 100%;
-            flex-direction: column;
-          }
-        }
-      `}</style>
-
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
-
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
+      </div>
     </div>
+    <style jsx>{`
+       #customers {
+        font-family: Arial, Helvetica, sans-serif;
+        border-collapse: collapse;
+        width: 100%;
+      }
+      
+      #customers td, #customers th {
+        border: 1px solid #ddd;
+        padding: 8px;
+      }
+      
+      #customers tr:nth-child(even){background-color: #f2f2f2;}
+      
+      #customers tr:hover {background-color: #ddd;}
+      
+      #customers th {
+        padding-top: 12px;
+        padding-bottom: 12px;
+        text-align: left;
+        background-color: #04AA6D;
+        color: white;
+      }
+      `}</style>
+  </div>
   )
 }
